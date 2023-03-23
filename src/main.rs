688f1c06ae1e981d17275abada2c2b15f6c40c18
Colin Johnson <colin@colinjohnson.net>
2023-03-22 20:03:49 -0700

    Use show_image crate to display image

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 
 use clap::Parser;
 use num::Complex;
-use std::{fmt::Write};
 use image::{Rgb, RgbImage};
 use indicatif::{ProgressBar, ProgressStyle};
+use show_image::{create_window, event};
 
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Output file path to use instead of the Image preview window.
-    path: Option<std::path::PathBuf>,
+    #[arg(short, long)]
+    output: Option<String>,
 
     /// Width of the generated image.
     #[arg(short, long, default_value_t = 1000)]
@@ -45,34 +46,64 @@ struct Args {
     workers: u32,
 }
 
+#[show_image::main]
 fn main() {
     let args = Args::parse();
     let offset = Complex::new(args.real_offset, args.complex_offset);
     let center = Complex::new(args.x_res as f64, args.y_res as f64) / args.zoom / 2f64;
+
     let progress_bar = build_progress_bar((args.x_res * args.y_res) as u64);
+    progress_bar.set_message("Sampling Mandelbrot");
 
     let mut image = RgbImage::new(args.x_res, args.y_res);
     for x in 0..args.x_res {
         for y in 0..args.y_res {
-            let location: Complex<f64> = pixel_to_complex((x, y), center, offset, args.zoom);
-            let color = match sample_mandelbrot(location, args.threshold, args.max_iterations) {
-                Some(iterations) => iterations_to_color(iterations, args.max_iterations),
-                None => Rgb([0, 0, 0])
-            };
+            let color = get_pixel(&args, offset, center, x, y);
             image.put_pixel(x, y, color);
             progress_bar.inc(1);
         }
     }
 
+    match args.output {
+        Some(output) => {
+            progress_bar.set_message("Saving image");
+            image.save(output).unwrap();
+        },
+        None => {
+            progress_bar.set_message("Displaying image");
+            show_image(image).unwrap()
+        }
+    };
+
     progress_bar.set_message("Saving image");
-    image.save("output.png").unwrap();
-
-    // match args.path {
-    //     Some(pathBuf) => image.save(pathBuf.),
-    //     None =>
-    // }
-
     progress_bar.finish();
+}
+
+fn get_pixel(args: &Args, offset: Complex<f64>, center: Complex<f64>, x: u32, y: u32) -> Rgb<u8> {
+    let location: Complex<f64> = pixel_to_complex((x, y), center, offset, args.zoom);
+    let color = match sample_mandelbrot(location, args.threshold, args.max_iterations) {
+        Some(iterations) => iterations_to_color(iterations, args.max_iterations),
+        None => Rgb([0, 0, 0])
+    };
+    color
+}
+
+
+fn show_image(image: RgbImage)-> Result<(), Box<dyn std::error::Error>> {
+    let window = create_window("Mandelbrot", Default::default())?;
+    window.set_image("Mandelbrot", image)?;
+
+    // wait until escape button pressed
+    for event in window.event_channel()? {
+        if let event::WindowEvent::KeyboardInput(event) = event {
+            if event.input.key_code == Some(event::VirtualKeyCode::Escape)
+                && event.input.state.is_pressed() {
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn build_progress_bar(len: u64) -> ProgressBar {
@@ -80,7 +111,7 @@ fn build_progress_bar(len: u64) -> ProgressBar {
     progress_bar.set_style(
         ProgressStyle::with_template("{msg} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len}")
             .unwrap()
-            .progress_chars("#-")
+            .progress_chars("#>-")
     );
     progress_bar
 }
